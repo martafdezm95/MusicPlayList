d14edbfdaf1cdbf84987e4381bdcb001f29d252f
martafdezm95 <martafdezm95@gmail.com>
2016-11-11 19:24:52 +0100

    Learning the basics

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.js\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+var http = require("http");
+var url = require("url");
+
+function iniciar(route, handle) {
+  function onRequest(request, response) {
+    var dataPosteada = "";
+    var pathname = url.parse(request.url).pathname;
+    console.log("Peticion para " + pathname + " recibida.");
+
+    request.setEncoding("utf8");
+
+    request.addListener("data", function(trozoPosteado) {
+      dataPosteada += trozoPosteado;
+      console.log("Recibido trozo POST '" + trozoPosteado + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, dataPosteada);
+    });
+
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Servidor Iniciado");
+}
+
+exports.iniciar = iniciar;
