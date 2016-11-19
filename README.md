ac66000f45505fb02850da5d8aa4e53a31b66d67
Erik Bergkvist <bergkvej@clarkson.edu>
2016-11-19 14:06:13 +0100

    First Version
    first version

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# M7011E Project
-This is a web application for the course M7011E Design of Dynamic Web Systems at Luleå University of Technology.
-
-# Description
-The application mainly consists on an easy way of creating and managing your own music playlists.
+# SisTecWeb
+Practicas Sistemas y Tecnologias Web
--- models/mongo.js
+++ models/mongo.js
@@ -1,13 +1,29 @@
-var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/demoDb');
+/**
+ * Autor: Alejandro Solanas Bonilla
+ * NIA: 647647
+ * Fichero: mongo.js
+ * Fecha: 29/5/2016
+ * Funcion: Modelo de usuarios y notas de mongodb y conector de mongoose
+ */
 
+var mongoose    =   require("mongoose");
+mongoose.connect('mongodb://localhost:27017/stw6');
 // create instance of Schema
-var mongoSchema = mongoose.Schema;
+var mongoSchema =   mongoose.Schema;
 // create schema
-var userSchema = {
-    "userEmail" : String,
-    "userPassword" : String
+var userSchema  = {
+    "user" : String,
+    "pass" : String
+};
+
+var noteSchema = {
+    "fecha" : String,
+    "texto" : String,
+    "fichero": String
 };
 
 // create model if not exists.
-module.exports = mongoose.model('userLogin',userSchema);
+module.exports.User = mongoose.model('userLogin',userSchema);
+module.exports.Notes = mongoose.model('notes',noteSchema)
+
+
