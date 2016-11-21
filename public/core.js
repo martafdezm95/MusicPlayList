1d9e97c5d937439286e0395b9cfed1bd971cb3d9
Erik Bergkvist <bergkvej@clarkson.edu>
2016-11-21 18:25:08 +0100

    save install bower

--- public/core.js
+++ public/core.js
@@ -16,29 +16,29 @@ function mainController($scope, $http) {
     //     });
 
     // when submitting the add form, send the text to the node API
-    $scope.createUser = function() {
-        $http.post('/users', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.users = data.message;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+    // $scope.createUser = function() {
+    //     $http.post('/users', $scope.formData)
+    //         .success(function(data) {
+    //             $scope.formData = {}; // clear the form so our user is ready to enter another
+    //             $scope.users = data.message;
+    //             console.log(data);
+    //         })
+    //         .error(function(data) {
+    //             console.log('Error: ' + data);
+    //         });
+    // };
 
-    // delete a user after checking it
-    $scope.deleteUser = function(id) {
-        $http.delete('/users/' + id)
-            .success(function(data) {
-                $scope.users = data.message;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+    // // delete a user after checking it
+    // $scope.deleteUser = function(id) {
+    //     $http.delete('/users/' + id)
+    //         .success(function(data) {
+    //             $scope.users = data.message;
+    //             console.log(data);
+    //         })
+    //         .error(function(data) {
+    //             console.log('Error: ' + data);
+    //         });
+    // };
 
     //delete a note
     $scope.removeNote = function(n) {
