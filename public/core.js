7a3a660ec067ed8a48d5e24406280367e82030ce
Erik Bergkvist <bergkvej@clarkson.edu>
2016-11-25 19:39:41 +0100

    Added login modal view (need to fix close button)

--- public/core.js
+++ public/core.js
@@ -76,5 +76,23 @@ OnlineMusicLibrary.directive('myAudio', function() {
         }
     };
 });
-$("#somediv").load('login.html').dialog({modal:true});
 
+function openLoginModal()
+{
+    // Get the modal
+    var modal = document.getElementById('loginModal');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+}
